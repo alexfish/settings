4ea16701f7a81534d98f039eee442c245f101b1a
Alex Fish <alex@alexefish.com>
2013-01-12 22:34:26 +0000

    Adding Xcode themes and git ignore

--- install.sh
+++ install.sh
@@ -5,9 +5,14 @@ if [ -n "${1}" ]; then
   BASE_PATH=${1}
 fi
 
-echo -n "Installing Sublime Text 2: "
+echo -n "Installing Sublime Text 2 Packages: "
 mkdir -p "${HOME}/Library/Application Support/Sublime Text 2"
 cp -R "${BASE_PATH}/Sublime Text 2/" "${HOME}/Library/Application Support/Sublime Text 2/"
+echo "finished."
+
+echo -n "Installing Xcode Themes: "
+mkdir -p "${HOME}/Library/Developer/Xcode/UserData/FontAndColorThemes/"
+cp -R "${BASE_PATH}/Xcode/FontAndColorThemes/" "${HOME}/Library/Developer/Xcode/UserData/FontAndColorThemes/"
 echo "finished."
 
 echo -n "Installing bash prompt: "
